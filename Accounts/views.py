4ce08da0134e7d2038b51fdd9c6688e4bf2197af
Kerriea-star <ojared511@gmail.com>
2022-02-14 10:06:04 +0300

    Created our teacher signup and student signup views

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
+from Accounts.models import User, Student, Teacher
+from Accounts.forms import userSignUpForm
+from django.views.generic import CreateView
 
+# Creating signup for teacher and student
+class StudentSignUpView(CreateView):
+    model = User
+    form_class = userSignUpForm
+    template_name = "Accounts/studentSignup.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = "student"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.is_student=True
+            user.save()
+
+class TeacherSignUpView(CreateView):
+    model = User
+    form_Class = userSignUpForm
+    template_name = "Accounts/teacherSignup.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["user_type"] = "Teacher"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.is_teacher=True
+            user.save()
